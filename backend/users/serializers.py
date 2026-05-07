e2289c8955f0728de2aa8260cd996758835cdcd8
omerfarukcelik <faruk.omerc@gmail.com>
2026-05-07 17:37:09 -0400

    add auth user profile endpoint

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "username"]
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
