f6dc4039c94f22bc42366eeb9dab314394f597c8
omerfarukcelik <faruk.omerc@gmail.com>
2026-05-07 19:11:03 -0400

    add applications CRUD API

---
+++ backend/applications/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Application
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = [
+            "id",
+            "company",
+            "title",
+            "status",
+            "location",
+            "salary_range",
+            "applied_at",
+            "notes",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Application
+from .serializers import ApplicationSerializer
 
-# Create your views here.
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Application.objects.filter(user=self.request.user).order_by("-created_at")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
